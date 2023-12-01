8549614d049de109b709ec0089bf029256d8c24f
RohanCas <rolando.castro2023@alu.uct.cl>
2023-11-30 23:44:41 -0300

    update variables

---
+++ 1.-basic/variables.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main() {
+    int edad = 23;
+    double salario = 1500.75;
+    char esCierto = true;
+
+}
+    //tipos de enteros 
+    short enteroCorto = 10;
+    
